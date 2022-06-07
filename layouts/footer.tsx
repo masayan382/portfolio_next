0540602c5cfa37b80fc0121bc08f719af3942484
masayan382 <masan3821031@gmail.com>
2022-06-07 23:28:55 +0900

    footer+ロゴ

---
+++ layouts/footer.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Link from '@mui/material/Link'
+import { css } from '@mui/styled-engine'
+import Image from 'next/image'
+
+function Copyright() {
+    return (
+        <Typography css={whiteColor}>
+            <span>
+                <Image src="/rogo.png" alt="" width={14} height={14} />{' '}
+            </span>
+            {'Copyright © '}
+            Portfolio {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
+
+const footerArea = css({
+    backgroundColor: '#000',
+    textAlign: 'center',
+})
+const whiteColor = css({
+    color: '#fff',
+})
+
+export default function footer() {
+    return (
+        <Box
+            component="footer"
+            sx={{
+                py: 3,
+                px: 2,
+                mt: 'auto',
+            }}
+            css={footerArea}
+        >
+            <Container maxWidth="sm">
+                <Copyright />
+            </Container>
+        </Box>
+    )
+}
