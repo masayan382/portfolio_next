7b401e0ac17c6f44b2130349e0a4a044a79c2a64
masayan382 <masan3821031@gmail.com>
2022-06-14 23:35:10 +0900

    prticles2

--- components/ParticlesBackground.js
+++ components/ParticlesBackground.js
@@ -1,5 +1,6 @@
 import Particles from "react-tsparticles"
 import { loadFull } from "tsparticles"
+import { css } from "@mui/styled-engine"
 
 const ParticlesBackground = () => {
     const particlesInit = async (main) => {
@@ -20,76 +21,132 @@ const ParticlesBackground = () => {
             init={particlesInit}
             loaded={particlesLoaded}
             options={{
-                background: {
+                fpsLimit: 60,
+                particles: {
+                    number: {
+                        value: 200,
+                        limit: 300,
+                        density: {
+                            enable: true,
+                            value_area: 800,
+                        },
+                    },
                     color: {
-                        value: "#0d47a1",
+                        value: "#ffffff",
+                    },
+                    shape: {
+                        type: "circle",
+                        stroke: {
+                            width: 0,
+                            color: "#000",
+                        },
+                        polygon: {
+                            nb_sides: 5,
+                        },
+                        image: {
+                            src: "images/github.svg",
+                            width: 100,
+                            height: 100,
+                        },
+                    },
+                    opacity: {
+                        value: 0.5,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 1,
+                            opacity_min: 0.5,
+                            sync: false,
+                        },
+                    },
+                    size: {
+                        value: 30,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 10,
+                            size_min: 10,
+                            sync: false,
+                        },
+                    },
+                    line_linked: {
+                        enable: true,
+                        distance: 100,
+                        color: "#ffffff",
+                        opacity: 1,
+                        width: 1,
+                    },
+                    move: {
+                        enable: true,
+                        speed: 2,
+                        direction: "none",
+                        random: false,
+                        straight: false,
+                        out_mode: "out",
+                        attract: {
+                            enable: false,
+                            rotateX: 600,
+                            rotateY: 1200,
+                        },
                     },
                 },
-                fpsLimit: 120,
                 interactivity: {
+                    detect_on: "canvas",
                     events: {
+                        onHover: {
+                            enable: true,
+                            mode: "bubble",
+                            parallax: {
+                                enable: false,
+                                force: 60,
+                                smooth: 10,
+                            },
+                        },
                         onClick: {
                             enable: true,
                             mode: "push",
                         },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
                         resize: true,
                     },
                     modes: {
-                        push: {
-                            quantity: 4,
+                        grab: {
+                            distance: 400,
+                            lineLinked: {
+                                opacity: 1,
+                            },
+                        },
+                        bubble: {
+                            distance: 400,
+                            size: 100,
+                            duration: 2,
+                            opacity: 1,
+                            speed: 2,
                         },
                         repulse: {
                             distance: 200,
-                            duration: 0.4,
+                        },
+                        push: {
+                            particles_nb: 4,
+                        },
+                        remove: {
+                            particles_nb: 2,
                         },
                     },
                 },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    collisions: {
-                        enable: true,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
+                backgroundMask: {
+                    enable: true,
+                    cover: {
+                        color: {
+                            value: {
+                                r: 0,
+                                g: 0,
+                                b: 0,
+                            },
                         },
-                        random: false,
-                        speed: 6,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
                     },
                 },
-                detectRetina: true,
+                retina_detect: true,
+                fps_limit: 30,
             }}
         />
     )
