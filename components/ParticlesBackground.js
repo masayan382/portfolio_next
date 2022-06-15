773abaac998f901e7bfe5f74286869d9b70ef2d2
masayan382 <masan3821031@gmail.com>
2022-06-15 23:16:15 +0900

    top-page

--- components/ParticlesBackground.js
+++ components/ParticlesBackground.js
@@ -4,19 +4,25 @@ import { css } from "@mui/styled-engine"
 
 const ParticlesBackground = () => {
     const particlesInit = async (main) => {
-        console.log(main)
-
-        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
         await loadFull(main)
     }
+
+    const bg = css({
+        backgroundImage: "url('/img/bg.jpg')",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#323031",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+    })
 
     const particlesLoaded = (container) => {
         console.log(container)
     }
     return (
         <Particles
+            css={bg}
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
