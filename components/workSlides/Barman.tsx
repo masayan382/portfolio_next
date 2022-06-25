dcb85e7c18bbc51021170fe603cc40c3fb3d17ae
masayan382 <masan3821031@gmail.com>
2022-06-25 23:01:34 +0900

    card

--- components/workSlides/Barman.tsx
+++ components/workSlides/Barman.tsx
@@ -1,122 +1,63 @@
 import * as React from "react"
-import { styled } from "@mui/material/styles"
 import Card from "@mui/material/Card"
 import CardHeader from "@mui/material/CardHeader"
 import CardMedia from "@mui/material/CardMedia"
 import CardContent from "@mui/material/CardContent"
 import CardActions from "@mui/material/CardActions"
-import Collapse from "@mui/material/Collapse"
-import Avatar from "@mui/material/Avatar"
 import IconButton, { IconButtonProps } from "@mui/material/IconButton"
 import Typography from "@mui/material/Typography"
-import { red } from "@mui/material/colors"
-import FavoriteIcon from "@mui/icons-material/Favorite"
-import ShareIcon from "@mui/icons-material/Share"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import MoreVertIcon from "@mui/icons-material/MoreVert"
+import OpenInBrowserIcon from "@mui/icons-material/OpenInBrowser"
+import GitHubIcon from "@mui/icons-material/GitHub"
+import { css } from "@mui/styled-engine"
 
-interface ExpandMoreProps extends IconButtonProps {
-    expand: boolean
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-    const { expand, ...other } = props
-    return <IconButton {...other} />
-})(({ theme, expand }) => ({
-    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-        duration: theme.transitions.duration.shortest,
-    }),
-}))
+const cardTable = css({
+    width: "100%",
+    "tr > th": {
+        width: "30px",
+        whiteSpace: "nowrap",
+        textAlign: "left",
+        paddingRight: "8px",
+    },
+})
 
 const Barman: React.FC = () => {
-    const [expanded, setExpanded] = React.useState(false)
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded)
-    }
-
     return (
         <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-                avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                        1
-                    </Avatar>
-                }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                }
-                title="Shrimp and Chorizo Paella"
-                subheader="September 14, 2016"
-            />
+            <CardHeader title="Barman" />
             <CardMedia
                 component="img"
                 height="194"
-                image="/static/images/cards/paella.jpg"
-                alt="Paella dish"
+                image="/work/barman.png"
+                alt="barman"
             />
-            <CardContent>
+            <CardContent style={{ padding: "4px 16px" }}>
                 <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish and a fun
-                    meal to cook together with your guests. Add 1 cup of frozen
-                    peas along with the mussels, if you like.
+                    <table css={cardTable}>
+                        <tr>
+                            <th>概要</th>
+                            <td>
+                                架空の理容室のLP。パララックスデザインかつレスポンシブデザイン。フォームにはバリデーション機能あり。
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>公開日</th>
+                            <td>2019/09/30</td>
+                        </tr>
+                        <tr>
+                            <th>使用技術</th>
+                            <td>HTML、CSS、SCSS、javascript、jQuery</td>
+                        </tr>
+                    </table>
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
+                <IconButton aria-label="site">
+                    <OpenInBrowserIcon />
                 </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
+                <IconButton aria-label="github">
+                    <GitHubIcon />
                 </IconButton>
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </ExpandMore>
             </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add
-                        saffron and set aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large,
-                        deep skillet over medium-high heat. Add chicken, shrimp
-                        and chorizo, and cook, stirring occasionally until
-                        lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo
-                        in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-                        onion, salt and pepper, and cook, stirring often until
-                        thickened and fragrant, about 10 minutes. Add saffron
-                        broth and remaining 4 1/2 cups chicken broth; bring to a
-                        boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with
-                        artichokes and peppers, and cook without stirring, until
-                        most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                        heat to medium-low, add reserved shrimp and mussels,
-                        tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just
-                        tender, 5 to 7 minutes more. (Discard any mussels that
-                        don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes,
-                        and then serve.
-                    </Typography>
-                </CardContent>
-            </Collapse>
         </Card>
     )
 }
