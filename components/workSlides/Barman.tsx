2072e1bfe2049ef7186e10cbd27194c65c927586
masayan382 <masan3821031@gmail.com>
2022-06-26 14:58:20 +0900

    barman

--- components/workSlides/Barman.tsx
+++ components/workSlides/Barman.tsx
@@ -4,9 +4,12 @@ import CardHeader from "@mui/material/CardHeader"
 import CardMedia from "@mui/material/CardMedia"
 import CardContent from "@mui/material/CardContent"
 import CardActions from "@mui/material/CardActions"
-import IconButton, { IconButtonProps } from "@mui/material/IconButton"
+import IconButton from "@mui/material/IconButton"
 import WebIcon from "@mui/icons-material/Web"
 import GitHubIcon from "@mui/icons-material/GitHub"
+import Tooltip from "@mui/material/Tooltip"
+import Link from "@mui/material/Link"
+
 import { css } from "@mui/styled-engine"
 
 const cardTable = css({
@@ -58,12 +61,28 @@ const Barman: React.FC = () => {
                 </table>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="site">
-                    <WebIcon />
-                </IconButton>
-                <IconButton aria-label="github">
-                    <GitHubIcon />
-                </IconButton>
+                <Tooltip title="WEBサイトへ" placement="top">
+                    <Link
+                        href="https://masataka.site/barman/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <IconButton aria-label="site">
+                            <WebIcon />
+                        </IconButton>
+                    </Link>
+                </Tooltip>
+                <Tooltip title="Githubへ" placement="top">
+                    <Link
+                        href="https://masataka.site/barman/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <IconButton aria-label="github">
+                            <GitHubIcon />
+                        </IconButton>
+                    </Link>
+                </Tooltip>
             </CardActions>
         </Card>
     )
