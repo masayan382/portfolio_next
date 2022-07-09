b73fd8caab52c53f28d2e216b509850dc41a51a9
masayan382 <masan3821031@gmail.com>
2022-07-09 22:58:42 +0900

    リファクタリング

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,8 +26,13 @@ function MyApp(props: MyAppProps) {
 
     return (
         <CacheProvider value={emotionCache}>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1, width=device-width"
+                />
+            </Head>
             <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
                 <Component {...pageProps} />
             </ThemeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head"
 import { css } from "@mui/styled-engine"
 import { Header, Footer } from "../layouts/index"
 import { Slider } from "../layouts/index"
-import ParticlesBackground from "../components/ParticlesBackground"
 
 const contentArea = css({
     position: "relative",
@@ -12,30 +11,8 @@ const contentArea = css({
 const Home: NextPage = () => {
     return (
         <div css={contentArea}>
-            <Head>
-                <title>Portfolio</title>
-                <meta name="Portfolio" content="made by T.M" />
-                <link rel="icon" href="/favicon.png" />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-            </Head>
             <Header />
             <main>
-                {/* <ParticlesBackground /> */}
                 <Slider />
             </main>
             <Footer />
