9a610390b9c6fb5db5cc3dbe758499a7246f698b
masayan382 <masan3821031@gmail.com>
2022-07-24 15:38:38 +0900

    circle

--- components/skillCircles/Javascript.tsx
+++ components/skillCircles/Javascript.tsx
@@ -3,36 +3,29 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar"
 import { easeQuadInOut } from "d3-ease"
 import AnimatedProgressProvider from "./AnimatedProgressProvider"
 import "react-circular-progressbar/dist/styles.css"
+import ChangingProgressProvider from "./ChangingProgressProvider"
 
 const Javascript: React.FC = () => {
     const percentage: any = 85
     return (
         <div style={{ maxWidth: 160, minWidth: 64 }}>
-            <AnimatedProgressProvider
-                valueStart={0}
-                valueEnd={85}
-                duration={2.5}
-                easingFunction={easeQuadInOut}
-                repeat
-            >
-                {(value: any) => {
-                    const roundedValue = Math.round(value)
-                    return (
-                        <CircularProgressbar
-                            value={value}
-                            text="javascript"
-                            background
-                            styles={buildStyles({
-                                strokeLinecap: "butt",
-                                textColor: "#fff",
-                                pathColor: "#F7DF1E",
-                                backgroundColor: "#000",
-                                textSize: "14px",
-                            })}
-                        />
-                    )
-                }}
-            </AnimatedProgressProvider>
+            <ChangingProgressProvider values={[0, 85]}>
+                {(percentage: any) => (
+                    <CircularProgressbar
+                        value={percentage}
+                        text="javascript"
+                        background
+                        styles={buildStyles({
+                            pathTransitionDuration: 1,
+                            strokeLinecap: "butt",
+                            textColor: "#fff",
+                            pathColor: "#F7DF1E",
+                            backgroundColor: "#000",
+                            textSize: "14px",
+                        })}
+                    />
+                )}
+            </ChangingProgressProvider>
         </div>
     )
 }
